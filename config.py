52e9b1577eb0d5bea070403b1313b0d98eff9fd0
谷一铭 <81207933+goyiming@users.noreply.github.com>
2024-10-21 19:29:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,15 @@
 GITHUB_TOKEN = "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"  # 可选，用于增加 API 限制
 
 # Telegram 配置
-TELEGRAM_BOT_TOKEN = "7445994479:AAHz4Rb-_U7LU8zN7g4cdE-LqMDeLq-ubFo"
-TELEGRAM_CHAT_ID = -1002292162960  # 修改为整数类型
+TELEGRAM_BOT_TOKEN = "XXXXXXXXXX:XXXXXXX-_XXXXXXXXXXXXX-XXXXXXX-XXXX"
+TELEGRAM_CHAT_ID = -XXXXXXXXXXXXX  # 修改为整数类型 一般群组ID前面有一个减号
 
 # 监控配置
 KEYWORDS = ["漏洞", "CVE", "安全"]
-CHECK_INTERVAL = 60  # 每60秒检查一次
-SEND_INTERVAL = 70  # 每70秒发送一次
+CHECK_INTERVAL = 600  # 每600秒检查一次
+SEND_INTERVAL = 610  # 每610秒发送一次
 
 # 代理配置
-USE_PROXY = True
+USE_PROXY = True    如果不开启代理可改为False，主要是为了解决大陆无法访问github问题
 PROXY_URL = "http://127.0.0.1:10090"
 
-# 在文件末尾添加以下行
-STATS_INTERVAL = 3600  # 每小时发送一次统计信息
